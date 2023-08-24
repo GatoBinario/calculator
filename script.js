f34f10cf2412a6543a67abb6ec82f554ceddc8db
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-24 14:55:13 +0200

    Tweaked errors

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ let noContent = true;
 
 function suboperate(a, op, b) {
     switch(op) {
-        case "add":
-            return add(a ,b);
-        case "sub":
-            return substract(a, b);
-        case "mult":
-            return multiply(a, b);
-        case "divide":
-            return divide(a, b);
+        case "+":
+            return add(+a ,+b);
+        case "-":
+            return substract(+a, +b);
+        case "×":
+            return multiply(+a, +b);
+        case "÷":
+            return divide(+a, +b);
     }
 }
 
@@ -37,21 +37,79 @@ buttons.forEach(button => {
         let buttonContent = e.explicitOriginalTarget.textContent;
         switch (buttonContent) {
             case "Clear":
+                setDisplay("0")
+                noContent = true;
                 break;
             case "Del":
+                screen.textContent = screen.textContent.slice(0 ,-1) 
                 break;
             case "=":
                 operate();
                 break;
             default:
-                addToDisplay(e.explicitOriginalTarget.textContent);
+                let display = getDisplay();
+                let lastChar = display[display.length-1]
+                let lastIsANumber = !isNaN(lastChar);
+                let buttonContentIsANumber = !isNaN(buttonContent);
+                
+                if(lastIsANumber) {
+                    addToDisplay(buttonContent);
+                }
+                else {
+                    if(buttonContentIsANumber) {
+                        addToDisplay(buttonContent);
+                    }
+                    else{
+                        display = getDisplay();
+                        let newDisplay = display.slice(0, -1);
+                        newDisplay+=buttonContent;
+                        setDisplay(newDisplay)
+                    }
+                }
         }
     })
 });
 
+function areOperatorsInArray(array) {
+    for(let i = 0; i<array.length; i++) {
+        let e = array[i];
+        if (e === "+" || e === "-" || e === "×" || e === "÷") {
+            return true;
+        }
+    }
+    return false;
+}
+
 function operate() {
     let screenContent = getDisplay();
-    // let operationArray = screenContent.split(/[+-]+/g);
-    let operationArray = screenContent.split(/([+-/×/÷])/g);
-    console.log(operationArray)
+    let opArray = screenContent.split(/([+-/×/÷])/g);
+    console.log(opArray)
+
+    while(areOperatorsInArray(opArray)) {
+        for(let i = 0; i<opArray.length; i++) {
+            let e = opArray[i];
+            if (e === "×" || e === "÷") {
+                let result = suboperate(opArray[i-1], opArray[i], opArray[i+1])
+                opArray[i] = result;
+                opArray.splice(i-1, 1)
+                opArray.splice(i, 1)
+                console.log(opArray)
+            
+                break;
+            }
+        }
+        for(let i = 0; i<opArray.length; i++) {
+            let e = opArray[i];
+            if (e === "+" || e === "-") {
+                let result = suboperate(opArray[i-1], opArray[i], opArray[i+1])
+                opArray[i] = result;
+                opArray.splice(i-1, 1)
+                opArray.splice(i, 1)
+                console.log(opArray)
+                
+                break;
+            }
+        }
+    }
+    setDisplay(opArray[0])
 }
