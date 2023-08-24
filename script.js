2c3c2b8522093efddf0b3031f8e3229faa1450e2
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-24 12:49:41 +0200

    Checkpoint

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-function add(a, b) {
-    return a + b;
-}
+function add(a, b) { return a + b; }
+function substract(a, b) { return a - b; }
+function multiply(a, b) { return a * b; }
+function divide(a ,b) { return a / b; }
 
-function substract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a ,b) {
-    return a / b;
-}
+function addToDisplay(string) { screen.textContent += string; }
+function setDisplay(string) { screen.textContent = string; }
+function getDisplay() { return screen.textContent; }
 
 let a = 0;
 let op;
 let b = 0;
+let screen = document.querySelector(".screen");
 
 function operate(a, op, b) {
     switch(op) {
@@ -30,3 +24,21 @@ function operate(a, op, b) {
             return divide(a, b);
     }
 }
+
+let buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        let buttonContent = e.explicitOriginalTarget.textContent;
+        switch (buttonContent) {
+            case "Clear":
+                break;
+            case "Del":
+                break;
+            case "=":
+                operate();
+                break;
+            default:
+                addToDisplay(e.explicitOriginalTarget.textContent);
+        }
+    })
+});
